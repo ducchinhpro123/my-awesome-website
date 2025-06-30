f4b927ca59cdc3fc53b0bc8b1d7410ffc50f8592
C <chinh99562@donga.edu.vn>
2025-06-30 10:52:06 +0700

    Implement pagination

--- src/controller/ProductController.php
+++ src/controller/ProductController.php
@@ -18,8 +18,41 @@ class ProductController extends Controller
     public function productsPage()
     {
         $template = 'products.html.twig';
-        $products = $this->productRepository->findAll();
-        $this->args['products'] = $products;
+        $page = filter_input(INPUT_GET, "page", FILTER_VALIDATE_INT);
+
+        if ($page === false || $page < 0) {
+            $page = 0;
+        }
+
+        $totalItems = $this->productRepository->getTotalProductsNumber();
+        $itemsPerPage = $this->productRepository::ITEM_PER_PAGE;
+        $totalPages = ceil($totalItems / $itemsPerPage);
+        $startNumberItemDisplay = 1;
+        $endNumberItemDisplay = $itemsPerPage * ($page + 1);
+
+        if ($endNumberItemDisplay > $totalItems) {
+            $endNumberItemDisplay = $totalItems;
+            $startNumberItemDisplay = $endNumberItemDisplay;
+        } else if ($endNumberItemDisplay > $itemsPerPage) {
+            $startNumberItemDisplay = $endNumberItemDisplay - $itemsPerPage;
+        }
+
+        $productsPagination = $this->productRepository->getProductsPagination($page);
+        $this->args['products'] = $productsPagination;
+
+        $this->args['pageNumber'] = $page + 1;
+        $this->args['totalPages'] = $totalPages;
+        $this->args['totalItemsNumber'] = $totalItems;
+        $this->args['pagination'] = [
+            'current_page' => $page,
+            'has_previous' => $page + 1 > 1,
+            'has_next' => $page + 1 < $totalPages,
+            'totalPages' => $totalPages,
+            'totalItemPerPage' => $itemsPerPage,
+            'endNumberItemDisplay' => $endNumberItemDisplay,
+            'startNumberItemDisplay' => $startNumberItemDisplay
+        ];
+
         print $this->twig->render($template, $this->args);
     }
 
--- src/repository/ProductRepository.php
+++ src/repository/ProductRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace MyAwesomeWebsite\repository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
@@ -17,6 +18,28 @@ class ProductRepository extends EntityRepository
         $this->entityManager = OrmHelper::getEntityManager();
         $entityMetadata = $this->entityManager->getClassMetadata(Product::class);
         parent::__construct($this->entityManager, $entityMetadata);
+    }
+
+    public const ITEM_PER_PAGE = 12;
+
+    public function getTotalProductsNumber()
+    {
+        return $this->createQueryBuilder('p')
+            ->select('count(p.id)')
+            ->getQuery()
+            ->getSingleScalarResult(); // execute the query and return the single value ($count)
+    }
+
+    public function getProductsPagination($page = 0)
+    {
+        $dql = "SELECT p FROM MyAwesomeWebsite\model\Product p";
+        $query = $this->entityManager->createQuery($dql)
+                                     ->setHint(Paginator::HINT_ENABLE_DISTINCT, false)
+                                     ->setFirstResult(self::ITEM_PER_PAGE * $page)
+                                     ->setMaxResults(self::ITEM_PER_PAGE);
+        $paginator = new Paginator($query, true);
+        /* error_log("query count"); */
+        return iterator_to_array($paginator);
     }
 }
 
